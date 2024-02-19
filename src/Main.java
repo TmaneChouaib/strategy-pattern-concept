99aa6b47701bda284adf5a28850ea9185e5987b3
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-02-19 17:58:18 +0100

    Added main class to demonstrate the usage of Context class with different strategies

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import strategy.ConcreteStrategyA;
+import strategy.ConcreteStrategyB;
+import strategy.Strategy;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Context context = new Context();
+
+        Strategy strategy = new ConcreteStrategyA();
+        context.setStrategy(strategy);
+        String result = context.executeStrategy();
+
+        System.out.println("I am using the " + result);
+
+        System.out.println("---------------------------------------------------------------------------");
+
+        strategy = new ConcreteStrategyB();
+        context.setStrategy(strategy);
+        result = context.executeStrategy();
+
+        System.out.println("I am using the " + result);
+
+
     }
 }
